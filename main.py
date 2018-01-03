963806e3a18b8cb1a056619a6e9220d579e1e988
ivanchingli <chingli2017@gmail.com>
2018-01-03 10:35:40 -0800

    first implementation of RNN
    still have to tune hyperparameters

--- main.py
+++ main.py
@@ -1,110 +1,37 @@
-# Parts of code borrowed from Danijar Hafner (https://danijar.com/introduction-to-recurrent-networks-in-tensorflow/,
-#https://gist.github.com/danijar/61f9226f7ea498abce36187ddaf51ed5)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 29 18:03:40 2017
+
+@author: ivan
+
+Parts of code borrowed from J. H. Wei:
+https://jhwei.github.io/CMPS242_Machine_learning/docs/#/3/1
+(code from Danijar H. was not working...)
+"""
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import time
+import time, functools, gensim, sets, pdb, collections, tensorflow as tf, numpy as np
+from tensorflow.contrib import rnn
+
 t0 = time.time()
 
-import functools
-import tensorflow as tf
-import numpy as np
-import gensim
-import sets
-import pdb
-
 import logging
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',filename='log/main_log.log', level=logging.INFO)
-# get TF logger
-log = logging.getLogger('tensorflow')
-log.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh = logging.FileHandler('log/main_log.log')
-fh.setLevel(logging.DEBUG)
-fh.setFormatter(formatter)
-log.addHandler(fh)
+filename='log/main2_log.log'
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',filename=filename, level=logging.INFO)
+log=logging
 #This is how you log within Spyder
-#log.info('this is a test message')
+##log.info('this is a test message')
 
+import os
+import tempfile
+TEMP_FOLDER = tempfile.gettempdir()
+print('Folder "{}" will be used to save temporary dictionary and corpus.'.format(TEMP_FOLDER))
 
-tf.reset_default_graph()  #resets graph, for experimenting
-
-def lazy_property(function):
-    attribute = '_' + function.__name__
-
-    @property
-    @functools.wraps(function)
-    def wrapper(self):
-        if not hasattr(self, attribute):
-            setattr(self, attribute, function(self))
-        return getattr(self, attribute)
-    return wrapper
-
-
-class SequenceClassification:
-
-    def __init__(self, data, target, dropout, num_hidden=100, num_layers=3):
-        self.data = data
-        self.target = target
-        self.dropout = dropout
-        self.num_hidden = num_hidden
-        self.num_layers = num_layers
-        self.prediction
-        self.error
-        self.optimize
-
-    @lazy_property
-    def logit(self):
-        # Recurrent network.
-        cells = []
-        for _ in range(self.num_layers):
-            cell = tf.contrib.rnn.GRUCell(self.num_hidden)  # Or LSTMCell(num_units)
-            cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=1.0 - self.dropout)
-            cells.append(cell)
-        network = tf.contrib.rnn.MultiRNNCell(cells)
-        output, state = tf.nn.dynamic_rnn(network, self.data, dtype=tf.float32)
-        # Select last output.
-        output = tf.transpose(output, [1, 0, 2])
-        last = tf.gather(output, int(output.get_shape()[0]) - 1)
-        out_size = int(self.target.shape[1])
-        logit = tf.contrib.layers.fully_connected(last, out_size, activation_fn=None)
-       # Softmax layer.
-#        weight, bias = self._weight_and_bias(
-#            self._num_hidden, int(self.target.get_shape()[1]))
-#        prediction = tf.nn.softmax(tf.matmul(last, weight) + bias)
-        return logit
-
-    @lazy_property
-    def prediction(self):
-        prediction = tf.nn.softmax(self.logit)
-        return prediction
-
-    @lazy_property
-    def cost(self):
-#        cross_entropy = -tf.reduce_sum(self.target * tf.log(self.prediction))
-#        return cross_entropy
-        return tf.losses.softmax_cross_entropy(self.target, self.logit)
-
-    @lazy_property
-    def optimize(self):
-        learning_rate = 0.003
-        optimizer = tf.train.RMSPropOptimizer(learning_rate)
-        return optimizer.minimize(self.cost)
-
-    @lazy_property
-    def error(self):
-        mistakes = tf.not_equal(
-            tf.argmax(self.target, 1), tf.argmax(self.prediction, 1))
-        return tf.reduce_mean(tf.cast(mistakes, tf.float32))
-
-    @staticmethod
-    def _weight_and_bias(in_size, out_size):
-        weight = tf.truncated_normal([in_size, out_size], stddev=0.01)
-        bias = tf.constant(0.1, shape=[out_size])
-        return tf.Variable(weight), tf.Variable(bias)
-
+tf.reset_default_graph()  #resets graph
 
 
 import csv
@@ -117,14 +44,9 @@ def preprocess(readfilename, writefilename):
     line_num = 0
     next(reader)
     labels = []
-    messages=[]
-    #test_labels = []
-    
+    messages=[]    
     for row in reader:
-#        if line_num>24: #only run through a few rows
-#            return 1,2 
         line_num += 1
-        #print line_num
         if line_num % 500 == 0:
             print(line_num)
         temp_label = row[0]
@@ -134,9 +56,7 @@ def preprocess(readfilename, writefilename):
             labels.append(0)
         elif temp_label == 'HillaryClinton':
             labels.append(1)
-#        print(temp_text)
         words = TweetTokenizer().tokenize(temp_text)
-#        print(words)
         for word in words:
             if word.startswith('http'):
                 words[words.index(word)] = '<url>'
@@ -159,88 +79,182 @@ def preprocess(readfilename, writefilename):
                 temp_sentence += " " + temp_word
         temp_sentence += "\n"
         messages.append(temp_sentence.split())
-#        print(temp_sentence)
         writer.write(temp_sentence)
     writer.close()
     print("Preprocessing is done!")
     return labels, messages
-
-#def tokenize(readfilename, writefilename,size=50): #Tokenize with Word2Vec
-#    res=[]
-#    labels, messages = preprocess(readfilename, writefilename)
-#    model = gensim.models.Word2Vec(messages, min_count=1, size=size, iter=8)
-#    model.train(messages, total_examples=model.corpus_count, epochs=model.iter)
-#    for message in messages:
-#        res.append(np.array([np.array(model.wv[word]) for word in message]))
-#    return np.array(res), labels
     
 def tokenize(readfilename, writefilename,size=50): #Tokenize with Word2Vec, use np.append
     labels, messages = preprocess(readfilename, writefilename)
-    max_length = max([len(message) for message in messages])
-    res=[]
-#    res=np.empty([0, max_length ,50], dtype=float)
+    X_train_length = [len(message) for message in messages]
+    max_length = max(X_train_length)
     model = gensim.models.Word2Vec(messages, min_count=1, size=size, iter=8)
     model.train(messages, total_examples=model.corpus_count, epochs=model.iter)
-    for message in messages:
-        temp=np.empty([0, 50], dtype=float)
-        for word in message:
-            temp=np.append(temp,np.array([model.wv[word]]),axis=0)
-        while temp.shape[0]<max_length:
-            temp=np.append(temp,np.zeros((1, 50)),axis=0)
-        res.append(temp)
-    res=np.stack(res,axis=0)
-#    labels=[[(label==1)*[1,0]+(label==0)*[0,1] for _ in range(max_length)] for label in labels] 
-#Hillary is [1,0] while Trump is [0,1], this one repeats for each time step
+#Hillary is [1,0] while Trump is [0,1]
     labels=[(label==1)*[1,0]+(label==0)*[0,1] for label in labels] 
     labels=np.stack(labels,axis=0)
-    return res, labels
+    
+    vocab_list = []
+    for word_list in messages:
+        vocab_list += word_list
+    count = collections.Counter(vocab_list)
 
-def segment(readfilename, writefilename): #implement data segment later
-    tweets,labels = tokenize(readfilename,writefilename)
+    vocab_dict = dict()
+    embedding_matrix = np.empty((0, size), float)
+
+    for word in count:
+        vocab_dict[word] = len(vocab_dict)
+        embedding_matrix = np.vstack((embedding_matrix, model[word]))
+
+    res=[]
+    for message in messages:
+        temp=[]
+        for word in message:
+            temp.append(vocab_dict[word])
+        while len(temp)<max_length:
+            temp.append(0)
+        res.append(np.array(temp))
+    res=np.stack(res,axis=0)
+
+    return res, labels, X_train_length, vocab_dict, embedding_matrix
+
+def segment(readfilename, writefilename,**kwargs): #write into data segment (database object)
+    tweets,labels, X_train_length, vocab_dict, embedding_matrix = tokenize(
+            readfilename,writefilename,**kwargs)
     full = sets.core.dataset.Dataset(data=tweets,target=labels)
-    return full
-        
-def main():  #have this perform 10-fold validation
-#    tweets,labels = tokenize('train.csv','train_p.csv')
-#    i=0
-#    valid_size=len(labels)//10
-#    test_temp = np.array(tweets[valid_size*i:valid_size*(i+1)])
-#    test_labels_temp = np.array(labels[valid_size*i:valid_size*(i+1)])
-#    train_temp = np.array(tweets[valid_size*(i+1):])
-#    labels_temp = np.array(labels[valid_size*(i+1):])
-#    _, rows, row_size = train.data.shape
-#    rows = num_words
-#    row_size = num_train
-#    num_classes = train.target.shape[1]
-#    num_classes = 2
-#    row_size=50
-#    rows=len(labels_temp)
-    full=segment('train.csv','train_p.csv')
-    train, test = sets.Split(0.9)(full)
-    dataset_size, max_length, input_size = train.data.shape
-    num_classes = train.target.shape[1]
-    data = tf.placeholder(tf.float32, [None, max_length, input_size])
-    target = tf.placeholder(tf.float32, [None, num_classes])
-    dropout = tf.placeholder(tf.float32)
-    model = SequenceClassification(data, target, dropout)
-    sess = tf.Session()
-    sess.run(tf.initialize_all_variables())
-#    epoch=0
-    for epoch in range(10):
-        for _ in range(100):
-            batch = train.sample(10)
-            sess.run(model.optimize, {
-                data: batch.data, target: batch.target, dropout: 0.5})
-        error = sess.run(model.error, {
-            data: test.data, target: test.target, dropout: 1})
-        print('Epoch {:2d} error {:3.1f}%'.format(epoch + 1, 100 * error))#    sess.run(model.optimize, {data: train.data, target: train.target, dropout: 0.5})
-#    error = sess.run(model.error, {data: test.data, target: test.target, dropout: 1})
-#    print('Epoch {:2d} error {:3.1f}%'.format(epoch + 1, 100 * error))
+    full.__setitem__('length',X_train_length)
+    return full, vocab_dict, embedding_matrix
 
-pdb.set_trace()
+# Training Parameters
+training_steps = 1
+batch_size = 93
+display_step = 50
+embed_size = 50
 
-if __name__ == '__main__':
-    main()
+full, vocab_dict, embedding_matrix = segment('train.csv','train_p.csv',size=embed_size)
+
+
+
+# Network Parameters
+num_input = 1
+time_step = full.data.shape[1]
+num_hidden = 20
+num_classes = 2
+
+# tf Graph input
+X = tf.placeholder(tf.int32, [None, time_step])
+X_length = tf.placeholder(tf.int32, [None])
+#embedding = tf.Variable(embedding_matrix)
+Y = tf.placeholder(tf.float16, [None, num_classes])
+
+# Define weights
+weights = {
+    'out': tf.Variable(tf.random_normal([num_hidden, num_classes]))
+}
+biases = {
+    'out': tf.Variable(tf.random_normal([num_classes]))
+}
+
+def RNN(x,x_length,weights,biases):
+    """x: rank-1, x_length: rank-0, weights: rank-2, biases: rank-1
+    rtype: rank-1"""
+    batch_size_tmp = tf.shape(x)[0]
+    embedding = tf.get_variable('embedding', [len(vocab_dict), embed_size])
+    embed = [tf.nn.embedding_lookup(embedding, row)
+             for row in tf.split(x, batch_size)]
+    embed = tf.reshape(embed, (batch_size_tmp, time_step, embed_size))
+    embed = tf.unstack(embed, time_step, 1)
+    
+    lstm_cell = rnn.BasicLSTMCell(num_hidden)
+    cell = tf.contrib.rnn.DropoutWrapper(lstm_cell, output_keep_prob=0.5)
+    cell = rnn.MultiRNNCell([cell] * 1)
+    outputs, states = rnn.static_rnn(
+            cell, dtype=tf.float32, sequence_length=x_length, inputs=embed)
+    
+    outputs = tf.stack(outputs)
+    outputs = tf.transpose(outputs, [1, 0, 2])
+
+    index = tf.range(0, batch_size_tmp) * \
+        full.data.shape[1] + tf.reshape(x_length - 1, [batch_size_tmp])
+    outputs = tf.gather(tf.reshape(outputs, [-1, num_hidden]), index)
+
+    return tf.matmul(outputs, weights['out']) + biases['out']
+
+logits = RNN(X, X_length, weights, biases)
+prediction = tf.nn.softmax(logits)
+tf.summary.histogram('logits', logits)
+
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits=logits, labels=Y))
+
+optimizer = tf.train.AdamOptimizer()
+train_op = optimizer.minimize(loss_op)
+tf.summary.scalar('loss', loss_op)
+
+correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+tf.summary.scalar('accuracy', accuracy)
+
+init = tf.global_variables_initializer()
+
+merged_summary = tf.summary.merge_all()
+
+
+# Start training
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+
+log.info('starting training for the follwing parameters: '
+#log.info('starting training for the follwing parameters: '
+         +'training_steps='+str(training_steps)
+         +', batch_size='+str(batch_size)
+         +', embed_size='+str(embed_size)
+         +', num_hidden='+str(num_hidden))
+predict_all = []
+with tf.Session(config=config) as sess:
+    sess.run(init)
+    train,validation = sets.Split(0.9)(full) #equivalent to first iteration of 10-fold CV
+    for step in range(1, training_steps + 1): 
+        for i in range(1, train.data.shape[0] // batch_size + 1): #stochastic
+            batch=train.sample(batch_size)
+            batch_x = batch.data
+            batch_y = batch.target
+            batch_x_length = batch.length
+            batch_x_length = batch_x_length.reshape((-1))
+            summary, _ = sess.run([merged_summary, train_op], feed_dict={
+                    X: batch_x, X_length: batch_x_length, Y: batch_y})
+        loss = []
+        acc = []
+        for i in range(1, train.data.shape[0] // batch_size + 1):
+            batch=train.sample(batch_size)
+            batch_x = batch.data
+            batch_y = batch.target
+            batch_x_length = batch.length
+            batch_x_length = batch_x_length.reshape((-1))
+            loss_tmp, acc_tmp = sess.run([loss_op, accuracy], feed_dict={X: batch_x, X_length: batch_x_length,
+                                                                         Y: batch_y})
+            loss.append(loss_tmp)
+            acc.append(acc_tmp)
+        log.info("Step " + str(step) + ", Minibatch Loss= " +
+              "{:.4f}".format(np.mean(loss)) + ", Training Accuracy= " +
+              "{:.3f}".format(np.mean(acc))) 
+        validation_loss=[]
+        validation_acc=[]
+        for i in range(1, validation.data.shape[0]//batch_size+1):
+            batch=validation.sample(batch_size)
+            batch_x = batch.data
+            batch_y = batch.target
+            batch_x_length = batch.length
+            batch_x_length = batch_x_length.reshape((-1))
+            loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x, X_length: batch_x_length,
+                                                                             Y: batch_y})
+            validation_loss.append(loss)
+            validation_acc.append(acc)
+        log.info("Step " + str(step) + ", Validation Loss= " +
+              "{:.4f}".format(np.mean(validation_loss)) + ", Validation Accuracy= " +
+              "{:.3f}".format(np.mean(validation_acc)))
+print("Optimization Finished!")
+
     
 t1 = time.time()
-print('Code run-time: ',t1-t0,'seconds')
+log.info('Code run-time: '+str(t1-t0)+' seconds')
